34f7e1c68ae2520cac7c0b20894b3d47967ea339
luca <lucadonofrio@gmail.com>
2025-02-03 10:00:27 -0300

    first commit

---
+++ src/main/java/varahas/main/VarahasApplication.java
@@ -0,0 +1,13 @@
+package varahas.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarahasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarahasApplication.class, args);
+	}
+
+}
