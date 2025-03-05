5a94d0bc1f84363089708c9f4f73d09a216ef035
luca <lucadonofrio@gmail.com>
2025-03-05 11:50:59 -0300

    productos

---
+++ src/main/java/varahas/main/dto/ProductDTO.java
@@ -0,0 +1,43 @@
+package varahas.main.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import varahas.main.entities.Product;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+	private Long id;
+	private String name;
+	private String description;
+	private BigDecimal price;
+	private Integer stock;
+	private Integer mercadoLibreId;
+	private boolean isOnMercadoLibre;
+	private boolean isOnTiendaNube;
+	
+	public static List<ProductDTO> fromList(List<Product> prods) {
+		return prods.stream().map(ProductDTO::from).toList();
+    }
+	
+	public static ProductDTO from(Product prod) {
+		return ProductDTO.builder()
+                .id(prod.getId())
+                .name(prod.getName())
+                .description(prod.getDescription())
+                .price(prod.getPrice())
+                .stock(prod.getStock())
+                .mercadoLibreId(prod.getMercadoLibreId())
+                .isOnMercadoLibre(prod.isOnMercadoLibre())
+                .isOnTiendaNube(prod.isOnTiendaNube())
+                .build();
+	}
+}
---
+++ src/main/java/varahas/main/exceptions/IllegalTennantAccessException.java
@@ -0,0 +1,12 @@
+package varahas.main.exceptions;
+
+public class IllegalTennantAccessException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public IllegalTennantAccessException(String message) {
+		super(message);
+		
+	}
+
+}
