b79e0070720766d10afe3e29b751d7037ea1d833
luca <lucadonofrio@gmail.com>
2025-06-27 15:39:42 -0300

    fix

--- src/main/java/varahas/main/services/TenantService.java
+++ src/main/java/varahas/main/services/TenantService.java
@@ -3,6 +3,7 @@ package varahas.main.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import varahas.main.dto.TnAuthDto;
 import varahas.main.entities.Tenant;
 import varahas.main.repositories.TenantRepository;
 
@@ -28,6 +29,15 @@ public class TenantService {
 	public Tenant findByMlUserId(String userId) {
 		return this.tenantRepository.findByMlUserId(userId)
 				.orElseThrow(() -> new RuntimeException("Tenant not found for user_id: " + userId));
+	}
+
+	public void setTnData(Tenant tenant, TnAuthDto authData) {
+		if (tenant == null || authData == null) {
+			throw new IllegalArgumentException("Tenant or authData cannot be null");
+		}
+		tenant.setTiendaNubeAccessToken(authData.getAccessToken());
+		tenantRepository.save(tenant);
+		
 	}
 
 }
